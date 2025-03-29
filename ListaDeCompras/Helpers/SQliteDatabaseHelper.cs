d5d171f27f537ecb56406c267d1307b5ab5276e2
kaua <kauabenjamint@gmail.com>
2025-03-29 15:49:30 -0300

    Agenda 2

--- ListaDeCompras/Helpers/SQliteDatabaseHelper.cs
+++ ListaDeCompras/Helpers/SQliteDatabaseHelper.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ListaDeCompras.Models;
+using SQLite;
 
 namespace ListaDeCompras.Helpers
 {
-    class SQliteDatabaseHelper
+    public class SQliteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _connection;
+
+        public SQliteDatabaseHelper(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Produto>().Wait();
+
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _connection.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+
+            return _connection.QueryAsync<Produto>(
+                sql, p.Descricao, p.Quantidade, p.Preco, p.Id
+            );
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> getAll() 
+        {
+            return _connection.Table<Produto>().ToListAsync();
+        }
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%?%"+ q +"%'";
+
+            return _connection.QueryAsync<Produto>(sql);
+        }
     }
 }
